40f234749104f6f75e89ab88d6c1656ab93a3430
Duncan Mac Leod <dnmacusa@gmail.com>
2020-07-24 00:59:29 -0700

    Pokemon lookup added and informatin about pokemon displays

--- src/components/pokedex/RightScreen.js
+++ src/components/pokedex/RightScreen.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-const RightScreen = () => {
+const RightScreen = ({ pokemon }) => {
+  const convertHeight = (height) => {
+    let inches = height * 3.937;
+    let feet = Math.floor(inches / 12);
+    let remaining = inches % 12;
+    if (Math.round(remaining) === 12) {
+      feet = 1;
+      remaining = 0;
+    }
+    return `${feet}' ${Math.round(remaining)}''`;
+  };
+
+  const convertWeight = (weight) => {
+    let lbs = weight * 0.22;
+    return `${lbs.toFixed(1)} lbs`;
+  };
+
   return (
     <div className="right-screen">
-      <h3>Pokemon name</h3>
+      <h2>Name: {pokemon.name ? pokemon.name.toUpperCase() : "error"}</h2>
+      <h2>Height: {convertHeight(pokemon.height)}</h2>
+      <h2>Weight: {convertWeight(pokemon.weight)}</h2>
     </div>
   );
 };
